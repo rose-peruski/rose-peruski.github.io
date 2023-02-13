3152918035173127dd98571d3140c837a981cc7f
rose-peruski <rose.peruski@gmail.com>
2023-02-13 16:17:21 -0500

    added css and js files

---
+++ csun23.js
@@ -0,0 +1,52 @@
+
+
+function slide1View(){
+    let  x = document.getElementById("problemState");
+    x.classList.remove("hidden");
+    let y = document.getElementById("slide3");
+    y.classList.add("hidden");
+}
+
+function slide3View(){
+    let  x = document.getElementById("slide3");
+    x.classList.remove("hidden");
+    let y = document.getElementById("problemState");
+    y.classList.add("hidden");
+}
+
+
+
+function slide4View(){
+    let  x = document.getElementById("slide4");
+    x.classList.remove("hidden");
+    let y = document.getElementById("slide3");
+    y.classList.add("hidden");
+}
+
+function slide5View(){
+    let  x = document.getElementById("slide5");
+    x.classList.remove("hidden");
+    let y = document.getElementById("slide4");
+    y.classList.add("hidden");
+}
+
+function slide6View(){
+    let  x = document.getElementById("slide6");
+    x.classList.remove("hidden");
+    let y = document.getElementById("slide5");
+    y.classList.add("hidden");
+}
+
+function slide7View(){
+    let  x = document.getElementById("slide7");
+    x.classList.remove("hidden");
+    let y = document.getElementById("slide6");
+    y.classList.add("hidden");
+}
+
+function slide8View(){
+    let  x = document.getElementById("slide8");
+    x.classList.remove("hidden");
+    let y = document.getElementById("slide7");
+    y.classList.add("hidden");
+}
