5e66ccca18f91e4bd22ab44ea852b84fcac4ec96
Rose Peruski <rp95401@of060m4n22tw6ye.kroger.com>
2023-02-15 15:17:24 -0500

    updated JS, added thank you slide

--- csun23.js
+++ csun23.js
@@ -1,52 +1,75 @@
 
+ function previousSlide(id1, id2){
+    id1.classList.add('hidden');
+    id2.classList.remove('hidden');
+}
 
-function slide1View(){
-    let  x = document.getElementById("problemState");
+function nextSlide(x, y) {
+    x.classList.add('hidden');
+    y.classList.remove('hidden');
+}
+
+function startShow(){
+    let  x = document.getElementById("slide2");
     x.classList.remove("hidden");
-    let y = document.getElementById("slide3");
+    let y = document.getElementById("slide1");
     y.classList.add("hidden");
 }
 
-function slide3View(){
-    let  x = document.getElementById("slide3");
-    x.classList.remove("hidden");
-    let y = document.getElementById("problemState");
-    y.classList.add("hidden");
-}
+// function slide2View(){
+//     let  x = document.getElementById("slide3" );
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide2");
+//     y.classList.add("hidden");
+// }
+// function slide1View(){
+//     let  x = document.getElementById("problemState");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide3");
+//     y.classList.add("hidden");
+// }
+
+// function slide3View(){
+//     let  x = document.getElementById("slide3");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("problemState");
+//     y.classList.add("hidden");
+// }
 
 
 
-function slide4View(){
-    let  x = document.getElementById("slide4");
-    x.classList.remove("hidden");
-    let y = document.getElementById("slide3");
-    y.classList.add("hidden");
-}
+// function slide4View(){
+//     let  x = document.getElementById("slide4");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide3");
+//     y.classList.add("hidden");
+// }
 
-function slide5View(){
-    let  x = document.getElementById("slide5");
-    x.classList.remove("hidden");
-    let y = document.getElementById("slide4");
-    y.classList.add("hidden");
-}
+// function slide5View(){
+//     let  x = document.getElementById("slide5");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide4");
+//     y.classList.add("hidden");
+// }
 
-function slide6View(){
-    let  x = document.getElementById("slide6");
-    x.classList.remove("hidden");
-    let y = document.getElementById("slide5");
-    y.classList.add("hidden");
-}
+// function slide6View(){
+//     let  x = document.getElementById("slide6");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide5");
+//     y.classList.add("hidden");
+// }
 
-function slide7View(){
-    let  x = document.getElementById("slide7");
-    x.classList.remove("hidden");
-    let y = document.getElementById("slide6");
-    y.classList.add("hidden");
-}
+// function slide7View(){
+//     let  x = document.getElementById("slide7");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide6");
+//     y.classList.add("hidden");
+// }
 
-function slide8View(){
-    let  x = document.getElementById("slide8");
-    x.classList.remove("hidden");
-    let y = document.getElementById("slide7");
-    y.classList.add("hidden");
-}
+// function slide8View(){
+//     let  x = document.getElementById("slide8");
+//     x.classList.remove("hidden");
+//     let y = document.getElementById("slide7");
+//     y.classList.add("hidden");
+// }
+
